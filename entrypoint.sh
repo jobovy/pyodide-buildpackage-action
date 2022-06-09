5668223421762b6d99606593f93f9ecd55fe5872
Jo Bovy <jo.bovy@gmail.com>
2022-06-09 04:00:57 +0000

    First try

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+META_YML_PATH=$1
+PYODIDE_TAG=$2
+OUTPUT_DIR=$3
+PACKAGE_NAME=galpy
+#${GITHUB_REPOSITORY#*/}
+PACKAGE_URL=https://github.com/jobovy/galpy/archive/main.tar.gz
+#https://github.com/$GITHUB_REPOSITORY/archive/$GITHUB_SHA.tar.gz
+
+# Get pyodide and setup pyodide tools
+git clone https://github.com/pyodide/pyodide
+cd pyodide
+git checkout $2
+make
+cd ..
+
+# Put meta.yml in place
+mkdir pyodide/packages/$PACKAGE_NAME
+cp $1 pyodide/packages/$PACKAGE_NAME
+cd pyodide/packages/$PACKAGE_NAME
+sed -i '/url/c\url: $PACKAGE_URL' meta.yml
+cd ../../
+
+# Build
+python -m pyodide_build buildall --only '$PACKAGE_NAME' packages ../$OUTPUT_DIR
+
