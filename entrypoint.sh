94376a7d029bebd021be242c67e99dc426279b60
Jo Bovy <jo.bovy@gmail.com>
2022-06-10 03:04:06 +0000

    Some updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
+
+# Parse inputs 
 META_YAML_PATH=`realpath $1`
 PYODIDE_TAG=$2
 OUTPUT_DIR=`realpath $3`
-PACKAGE_NAME=galpy
-#${GITHUB_REPOSITORY#*/}
-PACKAGE_URL=https://github.com/jobovy/galpy/archive/main.tar.gz
-#https://github.com/$GITHUB_REPOSITORY/archive/$GITHUB_SHA.tar.gz
+if [ "$4" = "None" ]; then
+    PACKAGE_NAME=${GITHUB_REPOSITORY#*/}
+else
+    PACKAGE_NAME=$4
+fi
+PACKAGE_VERSION=$5
+if [ "$6" = "None" ]; then
+    if [ "$7" = "None" ]; then
+        PACKAGE_URL=https://github.com/GITHUB_REPOSITORY/archive/$GITHUB_SHA.tar.gz
+    else
+        PACKAGE_URL=$7
+    fi
+else
+    PACKAGE_URL=https://github.com/GITHUB_REPOSITORY/archive/$6.tar.gz
+fi
 
 # Get pyodide and setup pyodide tools
 git clone https://github.com/pyodide/pyodide
@@ -16,6 +29,7 @@ make
 # Put meta.yaml in place
 mkdir -p packages/$PACKAGE_NAME
 cp $META_YAML_PATH packages/$PACKAGE_NAME/meta.yaml
+sed -i 's@.*version.*@  version: '"$PACKAGE_VERSION"'@' packages/$PACKAGE_NAME/meta.yaml
 sed -i 's@.*url.*@  url: '"$PACKAGE_URL"'@' packages/$PACKAGE_NAME/meta.yaml
 sed -i '/sha256/d' packages/$PACKAGE_NAME/meta.yaml
 sed -i '/md5/d' packages/$PACKAGE_NAME/meta.yaml
