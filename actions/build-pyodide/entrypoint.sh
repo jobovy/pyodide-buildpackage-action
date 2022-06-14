2b23df491c2baf2301dffc4bf608ddccbd5e24b7
Jo Bovy <jo.bovy@gmail.com>
2022-06-14 15:38:11 -0400

    Not equal

--- actions/build-pyodide/entrypoint.sh
+++ actions/build-pyodide/entrypoint.sh
@@ -5,7 +5,7 @@ PYODIDE_TAG=$1
 PYODIDE_CACHE_HIT=$2
 
 # Get pyodide and setup pyodide tools
-if [ "$PYODIDE_CACHE_HIT" -ne "true" ];
+if [ "$PYODIDE_CACHE_HIT" != "true" ];
 then
     git clone https://github.com/pyodide/pyodide
     cd pyodide
